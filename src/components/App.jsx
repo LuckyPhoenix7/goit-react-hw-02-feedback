d567f1485cda6ca8c1128f00a890637a1140c1d2
LuckyPhoenix7 <a.lubashov@gmail.com>
2023-11-19 19:01:10 +0100

    HW#2(feedback) v1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,52 @@
-// Импорт jsx-файлов
-import { Profile } from './Profile';
-import { Statistics } from './Statistics';
-import { FriendList } from './FriendList';
-import { TransactionHistory } from './TransactionHistory';
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Statistics/Notification';
 
-// Импорт Json-документов
-import user from '../json/user.json';
-import data from '../json/data.json';
-import friends from '../json/friends.json';
-import transactions from '../json/transactions.json';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-export function App() {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+  onClickButton = value => {
+    this.setState(prevState => ({
+      ...prevState,
+      [value.toLowerCase()]: prevState[value.toLowerCase()] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Leave your feedback">
+          <FeedbackOptions onClickButton={this.onClickButton} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              stats={this.state}
+              total={this.countTotalFeedback()}
+              positive={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
